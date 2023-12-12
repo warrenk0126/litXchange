d00ff9bff46d476d47a82c289273976c9297d001
vts17 <vatjsv19@gmail.com>
2023-12-11 23:06:35 -0500

    library card changes

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,36 @@
+import {
+    Container,
+    Card,
+    Button,
+    Row,
+    Col
+  } from 'react-bootstrap';
+
+  <Row>
+            {userData.savedBooks?.map((book) => {
+              return (
+                <Col md="4">
+                  <Card key={book.bookId} border="dark">
+                    {book.image ? (
+                      <Card.Img
+                        src={book.image}
+                        alt={`The cover for ${book.title}`}
+                        variant="top"
+                      />
+                    ) : null}
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      <p className="small">Authors: {book.authors}</p>
+                      <Card.Text>{book.description}</Card.Text>
+                      <Button
+                        className="btn-block btn-danger"
+                        onClick={() => handleDeleteBook(book.bookId)}
+                      >
+                        Delete this Book!
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>

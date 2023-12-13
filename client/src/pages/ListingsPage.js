8b3ec12220518c3af512c240be5980d5e8a864fe
vts17 <vatjsv19@gmail.com>
2023-12-12 22:11:50 -0500

    updates to listing page - psuedo code

--- client/src/pages/ListingsPage.js
+++ client/src/pages/ListingsPage.js
@@ -1,3 +1,12 @@
 import React, { useState } from 'react';
 import { useMutation, gql } from '@apollo/client';
 
+//psuedo code of the solution
+
+
+<React.Fragment> {
+    <booklist/>
+    <button onClick={() => addBook()}>Add Book</button>
+    <button onClick={() => deleteBook()}>Delete Book</button>
+    <button onClick={() => updateBook()}>Update Book</button>
+}</React.Fragment>

b568256c9cbf2c6e3c975cf0ab2b93eb5535d11f
warrenk0126 <warrenk0126@gmail.com>
2023-12-14 15:57:24 -0800

    Added SignUp component and route in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import App from './App';
 import client from './apolloClient';
+import SignUp from './components/SignUp'; // Import the SignUp component
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Router>
-        <App />
+        <Switch>
+          <Route path="/signup" component={SignUp} /> {/* Add a route for SignUp */}
+          <Route path="/" component={App} />
+        </Switch>
       </Router>
     </ApolloProvider>
   </React.StrictMode>,
